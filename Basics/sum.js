a5b9220b11e5ca2f0025fa08ae6c37af54c75be0
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-05-09 09:39:33 +0545

    Add files via upload

---
+++ Basics/sum.js
@@ -0,0 +1,14 @@
+let a, b, c;
+a=10;
+b=113;
+c= a+b;
+
+console.log("Sum of and b = "+c);
+
+
+
+/*
+var and let: You can freely change the value stored in these variables after they are declared.
+const: Stands for constant, and rightly so! Once you assign a value to a const variable, 
+       it becomes locked in. You cannot reassign a new value to it.
+*/
