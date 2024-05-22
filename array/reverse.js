8d94c90df11de32ec3e5cf45331a5a2432e7186a
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-05-22 12:39:58 +0530

    Add files via upload

---
+++ array/reverse.js
@@ -0,0 +1,10 @@
+let arr = [1,2,3,4,5,6,7,8,9];
+console.log("Original array is = "+arr);
+
+let new_arr = arr.reverse();
+console.log("Reversed array is = "+new_arr);
+
+// let's check whether new array contain 0 or not:
+
+console.log(new_arr.includes(0));
+// ans is false.
