8db8b8c4cc3f6ef8c50fb747e22ba5fafa9c04d7
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-05-22 12:38:02 +0530

    Add files via upload

---
+++ array/add2array.js
@@ -0,0 +1,7 @@
+let arr1 = [1,2,5,8,'a'];
+let arr2 = ['b',5,200,2150];
+
+let arr_new = arr1.concat(arr2);
+console.log("First array = "+ arr1);
+console.log("Second array = ", arr2);
+console.log("new Array = "+arr_new);
